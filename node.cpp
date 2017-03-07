f9739d7ea5bd35e3ea1e1aaa409b03ab27c4d9b2
glauberrleite <contact@glauberrleite.com>
2017-03-07 01:49:32 -0300

    node class

---
+++ node.cpp
@@ -0,0 +1,8 @@
+#include"node.h"
+
+Node::Node(int row, int column){
+  this->row = row;
+  this->column = column;
+
+  // calculate minimax
+}
---
+++ node.h
@@ -0,0 +1,8 @@
+class Node {
+public:
+  Node(int row, int column);
+private:
+  int minimax;
+  int row;
+  int column;
+};
